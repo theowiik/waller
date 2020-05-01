7275a96786ed307b28166aa9fcbfe546778cb6ee
theowiik <42991003+theowiik@users.noreply.github.com>
2020-05-02 00:01:00 +0200

    Set hardcoded wallpaper

--- waller.py
+++ waller.py
@@ -1,4 +1,11 @@
-def main():
-  print('Hello world!')
+import ctypes
 
-main()
+PATH = 'D:\\GitHub\\wallpapers\\wallpapers\\wallpaper.jpg'
+SPI_SETDESKWALLPAPER = 20
+
+
+def change_bg(path):
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, PATH, 3)
+
+
+change_bg(PATH)
