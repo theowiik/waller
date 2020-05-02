70ff1733a76dbc71cf7bc628e20cc0916df2a299
theowiik <42991003+theowiik@users.noreply.github.com>
2020-05-02 18:45:55 +0200

    Set wallpaper to random one in wallpaper folder

--- waller.py
+++ waller.py
@@ -1,23 +1,38 @@
 import ctypes
 import platform
+import os
+import random
 
-PATH = 'D:\\GitHub\\wallpapers\\wallpapers\\wallpaper.jpg'
-SPI_SETDESKWALLPAPER = 20
+# Windows constant for changing wallpapers
+WINDOWS_SPI_SET_WALLPAPER = 20
+
+# The name of the wallpaper folder
+WALLPAPER_FOLDER = 'wallpapers'
 
 
-def change_bg(path):
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, PATH, 3)
+def change_bg(path: str) -> None:
+    ctypes.windll.user32.SystemParametersInfoW(WINDOWS_SPI_SET_WALLPAPER, 0, path, 3)
 
 
-os = platform.system()
+def get_random_local_image() -> str:
+    """
+    :returns the path to a random image locally.
+    """
+    images = os.listdir(WALLPAPER_FOLDER)
+    image = random.choice(images)
 
-if os == 'Windows':
+    return os.path.abspath(WALLPAPER_FOLDER + '/' + image)
+
+
+system = platform.system()
+
+if system == 'Windows':
     print('Windows')
-elif os == 'Darwin':
+elif system == 'Darwin':
     print('MacOS')
-elif os == 'Linux':
+elif system == 'Linux':
     print('Linux')
 else:
     print('OS not supported')
 
-change_bg(PATH)
+change_bg(get_random_local_image())
