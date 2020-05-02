5029cb4c15d4e2c70df0e8414e7acd99d188adc4
theowiik <42991003+theowiik@users.noreply.github.com>
2020-05-02 10:36:37 +0200

    Find OS

--- waller.py
+++ waller.py
@@ -1,4 +1,5 @@
 import ctypes
+import platform
 
 PATH = 'D:\\GitHub\\wallpapers\\wallpapers\\wallpaper.jpg'
 SPI_SETDESKWALLPAPER = 20
@@ -7,5 +8,16 @@ SPI_SETDESKWALLPAPER = 20
 def change_bg(path):
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, PATH, 3)
 
+
+os = platform.system()
+
+if os == 'Windows':
+    print('Windows')
+elif os == 'Darwin':
+    print('MacOS')
+elif os == 'Linux':
+    print('Linux')
+else:
+    print('OS not supported')
 
 change_bg(PATH)
